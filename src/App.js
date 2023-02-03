245f9be92e31aa2e8c574bc2a530684bfcb86455
hs <kaos5930@gmail.com>
2023-02-03 21:49:17 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/home';
+import BaseLayout from './components/layout/BaseLayout';
+import BoardList from './components/board/board_list';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>My Shop Board </h1>
+      <Routes>
+        <Route path='/' element={<BaseLayout />}>
+          <Route index element={<Home />} />
+          <Route path='board' element={<BoardList />}>
+            <Route path='list/:currentPage' element={<BoardList />} />
+          </Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
